07b01a49b1e4df8e4260494ab6e56665352e7185
r3vn <giuseppe.corti@protonmail.com>
2020-10-03 17:24:09 +0200

    fixed usermod on setup_user.sh

--- scripts/setup_user.sh
+++ scripts/setup_user.sh
@@ -4,7 +4,7 @@
 adduser --disabled-password --gecos "" mobian
 
 # add mobian to groups
-usermod -a -G sudo,render,plugdev,video
+usermod -a -G sudo,render,plugdev,video mobian
 
 # set default pass to 1234, change it
 echo "mobian:1234" | chpasswd
