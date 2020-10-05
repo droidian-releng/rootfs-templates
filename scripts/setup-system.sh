16f5e0a81ae4965025ce96140ab31f768b797751
r3vn <giuseppe.corti@protonmail.com>
2020-10-05 23:22:26 +0200

    imported changes from hybris-mobian/mobian-recipes, updated repository and gpg key
    Co-authored-by: erfanoabdi <erfangplus@gmail.com>

---
+++ scripts/setup-system.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Setup hostname
+echo $1 > /etc/hostname
+
+# Generate locales (only en_US.UTF-8 for now)
+sed -i -e '/en_US\.UTF-8/s/^# //g' /etc/locale.gen
+locale-gen
+
+# Change plymouth default theme
+plymouth-set-default-theme mobian
+
+# Load phosh on startup if package is installed
+if [ -f /usr/bin/phosh ]; then
+    systemctl enable phosh.service
+fi
