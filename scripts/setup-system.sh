f2484f4bb765fee4e0bf6bfa856c28aab3e26787
Eugenio Paolantonio (g7) <me@medesimo.eu>
2022-11-27 02:42:17 +0100

    [scripts] setup-system: explictly set en_US.UTF8 as default locale
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -6,6 +6,7 @@ echo "127.0.0.1 $1" >> /etc/hosts
 # Generate locales (only en_US.UTF-8 for now)
 sed -i -e '/en_US\.UTF-8/s/^# //g' /etc/locale.gen
 locale-gen
+update-locale LANG=en_US.UTF8
 
 # Change plymouth default theme
 plymouth-set-default-theme mobian
