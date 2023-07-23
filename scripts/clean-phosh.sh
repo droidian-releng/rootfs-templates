f6ff00af52dfe8888fdd3a41464791796c1630b3
Bardia Moshiri <fakeshell@bardia.tech>
2023-07-23 18:08:23 -0400

    clean-phosh: initialize flathub during image creation

--- scripts/clean-phosh.sh
+++ scripts/clean-phosh.sh
@@ -4,4 +4,9 @@
 DEBIAN_FRONTEND=noninteractive apt-get remove -y totem
 DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
 
+# initialize flatpak
+if [ -f "/usr/bin/flatpak" ]; then
+   flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+fi
+
 exit 0
