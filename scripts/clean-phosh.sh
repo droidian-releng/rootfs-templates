a04022b840798da09b25ad5bdd9288e77b62bf75
Bardia Moshiri <fakeshell@bardia.tech>
2023-09-07 07:11:28 -0400

    clean-phosh: actually enable flathub

--- scripts/clean-phosh.sh
+++ scripts/clean-phosh.sh
@@ -7,6 +7,7 @@ DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
 # initialize flatpak
 if [ -f "/usr/bin/flatpak" ]; then
    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+   flatpak remote-modify --enable flathub
 fi
 
 exit 0
